0947df8d99b398e28f5c0906661d56eada1f0cae
PG <princegupta995643@gmail.com>
2025-07-16 18:16:13 +0530

    Basic Chatbot project commited

---
+++ src/langgraphagenticai/Nodes/basic_chatbot_node.py
@@ -0,0 +1,13 @@
+from src.langgraphagenticai.state.state import State
+class BasicChatbotNode:
+    """
+    Basic chatbot node implementation
+    """
+    def __init__(self,model):
+        self.llm=model
+
+    def process(self,state:State)->dict:
+        """
+        process the input and generate a chatbot response
+        """
+        return {'messages':self.llm.invoke(state['messages'])}
---
+++ src/langgraphagenticai/main.py
@@ -0,0 +1,41 @@
+import streamlit as st
+from src.langgraphagenticai.ui.steamlitui.loadui import LoadStreamlitUI
+from src.langgraphagenticai.LLM.groqllm import GroqLLM
+from src.langgraphagenticai.Graph.graph_builder import Graphbuilder
+from src.langgraphagenticai.ui.steamlitui.display_result import DisplayResultStreamlit
+
+def load_streamlit_agentic_ai_app():
+    """
+    Loads and runs the agentic ai application with streamlit UI. This function intializes the ui,
+    handel user inputs, configures the llm model , sets up the graph based on selected usecase
+    displays the output while implementing exception handling for robustness
+    """
+    ui=LoadStreamlitUI()
+    user_input=ui.load_streamlit_ui()
+    if not user_input:
+        st.error("Error: Falild to load user input from the UI")
+        return
+    user_message=st.chat_input("Enter your message")
+    if user_message:
+        try:
+            obj_llm_config=GroqLLM(user_controls_input=user_input)
+            model=obj_llm_config.get_llm_model()
+            if not model:
+                st.error("Error: LLM model could not be initialized")
+                return 
+            usecase=user_input.get("selected_usecase")
+            if not usecase:
+                st.error("Error: No usecase selected")
+                return 
+            
+            # Graph builder
+            graph_builder=Graphbuilder(model)
+            try:
+                graph=graph_builder.setup_graph(usecase)
+                DisplayResultStreamlit(usecase,graph,user_message).display_result_on_ui()
+            except Exception as e:
+                st.error(e)
+                return
+        except Exception as e:
+            print(f"Error:{e}")
+            return
